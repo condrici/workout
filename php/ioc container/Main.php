5ea015ef52201701568cb7a043fc82842ba30581
condrici <condrici@gmail.com>
2025-01-24 11:59:10 +0100

    Add ioc test

---
+++ php/ioc container/Main.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+require_once './vendor/autoload.php';
+
+use App\Src\ConfigInterface;
+Use App\Src\PHPConfig;
+use App\Src\App;
+use App\Src\Container;
+
+$container = new Container();
+$container->bind(ConfigInterface::class, PHPConfig::class);
+
+$app = $container->resolveConstructor(App::class, [
+    'arg1' => 'value1',
+    'arg2' => 'value2'
+]);
+
+$value = $container->resolveMethod($app, 'handle', [
+    'arg1' => 'value1',
+    'arg2' => 'value2'
+]);
+
+var_dump($value);
+
+/**
+ * Test 1: App class with injected dependencies in constructor
+ */
+
+echo PHP_EOL;
+print_r($app);
+echo PHP_EOL;
+
+/**
+ * Test 2: App class after executing its method with injected dependencies
+ */
+
+echo PHP_EOL;
+print_r($value);
+echo PHP_EOL;
