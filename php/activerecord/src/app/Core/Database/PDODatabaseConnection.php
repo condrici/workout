a26692d266c7eeaf0bebe036f7521cbb46e81e5a
condrici <condrici@gmail.com>
2025-02-11 12:28:58 +0100

    Add TODO

--- php/activerecord/src/app/Core/Database/PDODatabaseConnection.php
+++ php/activerecord/src/app/Core/Database/PDODatabaseConnection.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace App\Core\Database;
 
 use PDO;
+
+/**
+ * TODO: Revise abstract, it was not created properly
+ */
 interface PDODatabaseConnection
 {
     public static function getInstance(): PDO;
